ecf0c4cd4071e15dea105c9702ee9057e8e218da
Joel Turnbull <joelbywan@gmail.com>
2012-05-20 23:55:59 -0400

    feature tests for presence of a google calendar

---
+++ features/browse_calendar.feature
@@ -0,0 +1,6 @@
+Feature: Browse a Calendar
+  So that I can see a calendar
+
+  Scenario: See a Calander
+     When I am on the feeds page
+     Then I should see a calendar titled "Cincinnati Web Tech"
--- features/step_definitions/feed_steps.rb
+++ features/step_definitions/feed_steps.rb
@@ -10,6 +10,13 @@ Then /^I should see "([^"]*)"$/ do |name|
   page.should have_content name
 end
 
+Then /^I should see a calendar titled "([^"]*)"$/ do |name|
+  page_dom   = Nokogiri::HTML(page.body)
+  iframe_uri = page_dom.css('iframe').attribute('src')
+  iframe_dom = Nokogiri::HTML(open(iframe_uri))
+  iframe_dom.css('#calendarTitle').text.should == name 
+end
+
 When /^I fill in "([^"]*)" with "([^"]*)"$/ do |label, value|
   fill_in label, :with => value
 end
